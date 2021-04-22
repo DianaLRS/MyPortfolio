7bff9c74e3476c12f28f061a272a4d8c34aa4f6f
Luna <dlramos194@gmail.com>
2021-04-22 12:09:37 -0500

    adding projects section

--- src/page/ScrollPage.js
+++ src/page/ScrollPage.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import Navigation from '../components/Navigation'
 import Landing from '../sections/Landing'
-
 import SectionDividers from '../components/SectionDividers';
 import About from '../sections/About';
 import Projects from '../sections/Projects';
 import Contact from '../sections/Contact';
-
-
-// import '../assets/stylesheets/style.css'
 
 function ScrollPage(props) {
     return (
         <div>
             <Navigation/>
             <Landing/>
-            {/* <SectionDividers/> */}
             <About/>
             <SectionDividers/>
-            {/* <Projects/> */}
+            <Projects/>
             <SectionDividers/>
             <Contact/>
         </div>
